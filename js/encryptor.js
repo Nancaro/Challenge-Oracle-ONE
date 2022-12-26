4b4edae5060135e42238ba8040156d42b17ade19
Bruno Aguirre - Nancaro <97000958+Nancaro@users.noreply.github.com>
2022-12-25 21:31:42 -0300

    Desencriptador
    Desencriptador para challenge Alura usando HTML, CSS y JS

---
+++ js/encryptor.js
@@ -0,0 +1,99 @@
+const colorwhite = "rgb(222, 228, 250)"
+const colorblue = "rgba(10, 56, 113, 1)"
+let data = 1;
+
+const encryptContent = () => {
+    processedTextOnChange();
+    const decryptVar = document.querySelector(`.btn-decrypt`);
+    const encryptVar = document.querySelector(`.btn-encrypt`);
+    data = 1;
+    encryptVar.style.backgroundColor = colorblue;
+    encryptVar.style.color = colorwhite;
+    encryptVar.value = "Encriptando..."
+
+
+    decryptVar.style.backgroundColor = colorwhite;
+    decryptVar.style.color = colorblue;
+    decryptVar.value = "Desencriptar"
+
+    const comprobation = document.querySelector(`.textToProcessed`);
+    if (comprobation.value.length > 0) {
+        encrypt(comprobation.value);
+    }
+
+
+}
+
+const decryptContent = () => {
+    processedTextOnChange();
+    const decryptVar = document.querySelector(`.btn-decrypt`);
+    const encryptVar = document.querySelector(`.btn-encrypt`);
+
+    data = 2;
+
+
+    encryptVar.style.backgroundColor = colorwhite;
+    encryptVar.style.color = colorblue;
+    encryptVar.value = "Encriptar"
+
+    decryptVar.style.backgroundColor = colorblue;
+    decryptVar.style.color = colorwhite;
+    decryptVar.value = "Desencriptando..."
+
+
+    const comprobation = document.querySelector(`.textToProcessed`);
+    if (comprobation.value.length > 0) {
+        decrypt(comprobation.value);
+    }
+
+}
+
+const encrypt = (value) => {
+    const textArea = document.querySelector(`.processedText-textarea`);
+
+
+    let Encriptador = [/e/gi, /i/gi, /a/gi, /o/gi, /u/gi];
+    let palabrasClave = ["enter", "imes", "ai", "ober", "ufat"];
+    let newstr = value;
+    for (let i = 0; i < palabrasClave.length; i++) {
+        newstr = newstr.replace(Encriptador[i], palabrasClave[i]);
+    }
+    textArea.value = newstr;
+
+}
+
+const decrypt = (value) => {
+    const textArea = document.querySelector(`.processedText-textarea`);
+
+    let palabrasClave = [/enter/g, /imes/g, /ai/g, /ober/g, /ufat/g];
+    let desencriptador = ["e", "i", "a", "o", "u"];
+    let newstr = value;
+    for (let i = 0; i < palabrasClave.length; i++) {
+        newstr = newstr.replace(palabrasClave[i], desencriptador[i]);
+    }
+    textArea.value = newstr;
+}
+
+//
+
+const keyPressEvent = (e) => {
+    if (data === 1) {
+        encryptContent();
+    } else {
+        decryptContent();
+    }
+}
+
+
+const mainFunctional = () => {
+
+    const comprobation = document.querySelector(`.textToProcessed`);
+    comprobation.addEventListener('keyup', keyPressEvent);
+
+    const encryptVar = document.querySelector(`.btn-encrypt`);
+    const decryptVar = document.querySelector(`.btn-decrypt`);
+    encryptVar.addEventListener('click', encryptContent);
+    decryptVar.addEventListener('click', decryptContent);
+
+}
+mainFunctional();
---
+++ js/styles.js
@@ -0,0 +1,106 @@
+// Apartado de Funciones
+let repeat = 0;
+
+//función para insertar html en el cualquier parte de la pagina con solo un clase
+const insertHtml = (html, classs) => {
+    document.querySelector(`.${classs}`).innerHTML = html;
+}
+
+//función para cambiar el tipo de mensaje según la resolución de la pantalla
+const changeProcessedNoText = () => {
+    const noHTMLGreat = `
+    <section class="noContentGreat">
+        <div class="noCntent-div-img">
+            <img class="noCntent-img" src="img/search.svg" alt="search">
+        </div>
+        <div class="noContent-div-text">
+            <h2 class="noContent-h2">Ningún mensaje fue encontrado</h2>
+            <p class="noConten-p">Ingresa el texto que desees encriptar o desencriptar.</p>
+        </div>
+    </section>
+    `;
+    const noHTMLShort = `
+    <section class="noContentShort">
+        <div class="noContent-div-text">
+            <h2 class="noContent-h2">Ningún mensaje fue encontrado</h2>
+            <p class="noConten-p">Ingresa el texto que desees encriptar o desencriptar.</p>
+        </div>
+    </section>
+    `;
+    insertHtml("", "div-erase")
+
+    repeat = 0;
+    let width = screen.width;
+    if (width >= 1000) insertHtml(noHTMLGreat, `processedText`);
+    else insertHtml(noHTMLShort, `processedText`);
+
+}
+
+// función para cambiar entre en textarea para imprimir el texto y el mensaje de 
+const processedTextOnChange = () => {
+    const dataTextToProcessed = document.querySelector(`.textToProcessed`);
+    const yesHTML = `
+    <section class="yesContent">
+        <textarea class="processedText-textarea" placeholder="Texto procesado" readonly></textarea>
+
+        <input type="button" class="btn-copy btn btn2" value="Copiar"/>
+    </section>
+    `;
+    const noImgHTML = `
+    <img src="img/erase.svg" alt="x" class="erase" title="Borrar">
+    `;
+
+    if (dataTextToProcessed.value.trim()) {
+        if(repeat === 0){
+            repeat = 1
+            console.log(repeat)
+            insertHtml(yesHTML, `processedText`);
+            insertHtml(noImgHTML, "div-erase");
+            const copyText = document.querySelector(`.btn-copy`);
+            copyText.addEventListener('click', copy);
+            
+            
+            const erase = document.querySelector(`.erase`);
+            erase.addEventListener('click', eraseClick);
+        }
+    } else changeProcessedNoText();
+
+
+}
+
+const copy = () =>{
+
+    const content = document.querySelector('.processedText-textarea');
+    content.select();
+    document.execCommand('copy');
+
+    const copyText = document.querySelector(`.btn-copy`);
+    copyText.value = "Copiado";
+    copyText.style.backgroundColor = "rgba(10, 56, 113, 1)";
+    copyText.style.color = "rgb(222, 228, 250)";
+
+}
+
+const eraseClick = () => {
+    const dataTextToProcessed = document.querySelector(`.textToProcessed`);
+    const erase = document.querySelector(`.erase`);
+    const copyText = document.querySelector(`.btn-copy`);
+
+    dataTextToProcessed.value = "";
+    copyText.removeEventListener('onclick', copy);
+    erase.removeEventListener('onclick', eraseClick);
+    changeProcessedNoText();
+}
+
+//funcion main, aquí se ejecutan todas las funciones de arriba
+const main = () => {
+
+    changeProcessedNoText();
+
+    // Apartado de Eventos
+    const dataTextToProcessed = document.querySelector(`.textToProcessed`);
+    dataTextToProcessed.addEventListener('keyup', processedTextOnChange);
+
+}
+
+main();
